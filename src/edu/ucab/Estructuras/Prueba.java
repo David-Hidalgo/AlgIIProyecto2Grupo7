e71b9fc7021ab61789186e94907fc553c6d1ce26
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-07-05 21:05:42 -0400

    update Prueba.java

--- src/edu/ucab/Estructuras/Prueba.java
+++ src/edu/ucab/Estructuras/Prueba.java
@@ -1,21 +1,8 @@
 package edu.ucab.Estructuras;
 
+import com.AlgIIProyecto2Grupo7.Vehiculo;
 public class Prueba {
 	public static void main(String[] args) {
 		ArbolAVL arbol = new ArbolAVL();
-		arbol.insertar(10);
-		arbol.insertar(5);
-		arbol.insertar(13);
-		arbol.insertar(1);
-		arbol.insertar(6);
-		arbol.insertar(17);
-
-		System.out.println("Preorden");
-		arbol.preOrden(arbol.getRaiz());
-		System.out.println("Inorden");
-		arbol.inOrden(arbol.getRaiz());
-		System.out.println("Postorden");
-		arbol.postOrden(arbol.getRaiz());
-		
 	}
 }
