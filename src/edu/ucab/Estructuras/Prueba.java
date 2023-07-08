492605de8b4bab103fd484c8b159bbd353b2e8e8
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-07-08 17:38:17 -0400

    Co-authored-by: Daniel Castellanos <DanCas03@users.noreply.github.com>

--- src/edu/ucab/Estructuras/Prueba.java
+++
@@ -1,8 +0,0 @@
-package edu.ucab.Estructuras;
-
-import com.AlgIIProyecto2Grupo7.Vehiculo;
-public class Prueba {
-	public static void main(String[] args) {
-		ArbolAVL arbol = new ArbolAVL();
-	}
-}
