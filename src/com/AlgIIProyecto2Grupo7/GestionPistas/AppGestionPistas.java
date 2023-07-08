a046d07e52fa2fa189ce6c0075fa22f3d37a62d8
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-07-08 17:52:59 -0400

    update AppGestionPistas.java

--- src/com/AlgIIProyecto2Grupo7/GestionPistas/AppGestionPistas.java
+++ src/com/AlgIIProyecto2Grupo7/GestionPistas/AppGestionPistas.java
@@ -5,6 +5,6 @@ import com.AlgIIProyecto2Grupo7.Menu;
 public class AppGestionPistas {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Menu.menuGestionVehiculos();
+        Menu.menuPista();
     }
 }
