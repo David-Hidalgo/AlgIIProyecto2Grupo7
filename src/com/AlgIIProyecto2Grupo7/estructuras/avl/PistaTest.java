d0689b2f67a62a17984fa907dc4cf285cc3ce6bb
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-07-16 20:52:28 -0400

    bueno

--- src/com/AlgIIProyecto2Grupo7/estructuras/avl/PistaTest.java
+++ src/com/AlgIIProyecto2Grupo7/estructuras/avl/PistaTest.java
@@ -3,7 +3,6 @@ package com.AlgIIProyecto2Grupo7.estructuras.avl;
 public class PistaTest{
 	public static void main(String[] args) {
 		PistaAvl pista = Pista.crearPista(2);
-
 		Pista.generarTrampas(pista, 2, 2);
 		pista.getRaiz().inOrdenTrampas();
 		pista.inOrden();
